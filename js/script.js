e9800f697abe5dfae249cb0fd4dd4f3f2a4df85d
kaolaa <khatrach@gmail.com>
2018-11-12 01:30:51 +0000

    correction de l'erreur CROSS

--- js/script.js
+++ js/script.js
@@ -9,20 +9,20 @@ function init() {
             page: 1,
             pagesize: 5,
             selected: '',
-            test:1,
+            test: 1,
             search: "",
-            idshow:false,
+            idshow: false,
             searchlist: [],
-            rdisabled:0,
-            ldisabled:1,
+            rdisabled: 0,
+            ldisabled: 1,
             nbRestaurant: 0,
             FormAjout: [],
-            event :"",
-            name_e:"",
-            cuisine_e:"",
-            modifier:true,
-            id_e:""
-            
+            event: "",
+            name_e: "",
+            cuisine_e: "",
+            modifier: true,
+            id_e: ""
+
         },
         mounted() {
             console.log("--- MOUNTED, appelée avant le rendu de la vue ---");
@@ -31,35 +31,40 @@ function init() {
         },
         methods: {
             handleDelete(id) {
-                let url = "http://localhost:8080/api/restaurants" + id;
-
+                let url = "http://localhost:8080/api/restaurants/" + id;
+                this.name_e = " ";
+                this.id_e = " ";
+                this.cuisine_e = " ";
                 fetch(url, {
                     method: "DELETE",
                 })
-                .then(function(responseJSON) {
-                    responseJSON.json()
-                        .then(function(res) {
-                            // Maintenant res est un vrai objet JavaScript
-                            afficheReponseDELETE(res);
-                        });
+                    .then(function (responseJSON) {
+                      
+                        responseJSON.json()
+                            .then(function (res) {
+                                // Maintenant res est un vrai objet JavaScript
+
+                                // this.searchrest();
+
+                                afficheReponseDELETE(res);
+                            });
                     })
                     .catch(function (err) {
                         console.log(err);
-                });
+                    });
 
-              },
+            },
             handleCurrentChange(val) {
                 this.currentRow = val;
                 console.log(val);
-                this.name_e=val.name;
-                this.id_e=val._id;
-                this.cuisine_e=val.cuisine;
-                this.modifer=false;
+                this.name_e = val.name;
+                this.id_e = val._id;
+                this.cuisine_e = val.cuisine;
 
-              },
+            },
             testfct: function () {
 
-               this.test = this.test +1;
+                this.test = this.test + 1;
             },
 
             next: function () {
@@ -79,7 +84,7 @@ function init() {
             }
             ,
             // searchrest() {
-                
+
             //     this.searchlist=  this.restaurants.filter( (todo) => {  return todo.name.toLowerCase().includes(this.search.toLowerCase()) })
             //      console.log( this.searchlist);
             // },
@@ -87,7 +92,7 @@ function init() {
                 let url = "http://localhost:8080/api/restaurants?page=" + this.page + "&pagesize=" + this.pagesize;
 
                 fetch(url).then((data) => {
-                    console.log("les données sont arrivées !")
+                    // console.log("les données sont arrivées !")
                     return data.json();
                 }).then((dataEnJavaScript) => {
                     // ici on a bien un objet JS
@@ -101,37 +106,39 @@ function init() {
                 //this.restaurants.push(this.restaurants.length)
                 this.input = "";
             },
-            getRequest1: function() {
+            getRequest1: function () {
                 let url = "http://localhost:8080/api/restaurants";
 
                 fetch(url)
-                    .then(function(responseJSON)  {
+                    .then(function (responseJSON) {
                         responseJSON.json()
-                        .then(function(res) {
-                            // Maintenant res est un vrai objet JavaScript
-                            console.log("Debug : Request1");
-                            //console.log(res.data);
-                            this.Mytest();
-                            this.MyDebug(res);
-                            this.afficheReponseGET(res);
-                        });
+                            .then(function (res) {
+                                // Maintenant res est un vrai objet JavaScript
+                                console.log("Debug : Request1");
+                                //console.log(res.data);
+                                this.Mytest();
+                                this.MyDebug(res);
+                                this.afficheReponseGET(res);
+                            });
                     })
                     .catch(function (err) {
                         console.log(err);
-                });
+                    });
             },
-        },
+        }
+        ,
         computed: {
-            searchrest() {
-                // this.getDataFromWebService();
-                    return this.restaurants.filter((todo) => { 
-                        return todo.name.toLowerCase().includes(this.search.toLowerCase())
-                     })
-               
-                ;
-                
+            searchrest() {              
+                this.getDataFromWebService();
+                return this.restaurants.filter((todo) => {
+                    return todo.name.toLowerCase().includes(this.search.toLowerCase())
+                    
+                })
+
+                    ;
+
             }
-          }
+        }
 
     })
 }
@@ -139,42 +146,42 @@ function init() {
 function putRequest(event) {
     event.preventDefault();
 
-    
+
     let form = event.target;
-  
+
     let donneesFormulaire = new FormData(event.target);
 
     // let id = form._id.value; 
-    let id = form.id_e.value; 
+    let id = form.id_e.value;
     console.log(donneesFormulaire);
-    let url = "http://localhost:8080/api/restaurants" + id;
+    let url = "http://localhost:8080/api/restaurants/" + id;
 
     fetch(url, {
         method: "PUT",
         body: donneesFormulaire
     })
-    .then(function(responseJSON) {
-        responseJSON.json()
-            .then(function(res) {
-                // Maintenant res est un vrai objet JavaScript
-                afficheReponsePUT(res);
-                console.log(res.msg);
-            });
+        .then(function (responseJSON) {
+            responseJSON.json()
+                .then(function (res) {
+                    // Maintenant res est un vrai objet JavaScript
+                    afficheReponsePUT(res);
+                    console.log(res.msg);
+                });
         })
         .catch(function (err) {
             console.log(err);
-    });
+        });
 }
- //Div d'ajout 
- function postRequest(event) {
-           
+//Div d'ajout 
+function postRequest(event) {
+
     console.log(event);
     event.preventDefault(); //prevente default behaviour wich is reload
     //FormAjout.preventDefault();
-    
+
     let form = event.target; // recuperation du doc
-    
-    
+
+
     let donneesFormulaire = new FormData(form); //recup des données
     console.log(donneesFormulaire);
     let url = "http://localhost:8080/api/restaurants";
@@ -183,23 +190,23 @@ function putRequest(event) {
         method: "POST",
         body: donneesFormulaire
     })
-    .then(function(responseJSON) {
-        responseJSON.json()
-            .then(function(res) {
-                console.log(res.msg);
+        .then(function (responseJSON) {
+            responseJSON.json()
+                .then(function (res) {
+                    console.log(res.msg);
 
-                afficheReponsePOST(res);
-            });
+                    afficheReponsePOST(res);
+                });
         })
         .catch(function (err) {
             console.log(err);
-    });
+        });
 };
- function afficheReponsePOST(reponse) {
+function afficheReponsePOST(reponse) {
     let div = document.querySelector("#reponseAjout");
     div.innerHTML = reponse.msg;
 };
 // REQUETES DELETE
 function deleteRequest(event) {
-   
+
 }
